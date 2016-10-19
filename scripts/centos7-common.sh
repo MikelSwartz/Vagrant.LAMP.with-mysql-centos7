cd79a8e568f52ebf141e8f98f254e05bf4bab223
Mike Swartz <mikelswartz@gmail.com>
2016-10-19 08:46:49 -0400

    Enabled ssh passwords on private network

--- scripts/centos7-common.sh
+++ scripts/centos7-common.sh
@@ -3,3 +3,9 @@
 yum update -y --exclude=kernel
 yum install -y vim git unzip screen wget nc telnet net-tools
 
+#Enable ssh passwords (For testing)
+sed -e '/PasswordAuthentication\ no/ s/^#*/#/' -i /etc/ssh/sshd_config
+sed -e '/PasswordAuthentication\ yes/ s/^#*//' -i /etc/ssh/sshd_config
+
+systemctl restart sshd
+
